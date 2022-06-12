039eae06b9264d4e124c83a3e1c77cb3e71f2910
fdiazbmx <68572334+fdiazbmx@users.noreply.github.com>
2022-06-11 22:23:39 -0600

    Cambios en usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,13 @@ return new class extends Migration
             $table->string('Nom_Usuario')->comment("Nombre del Usuario");
             $table->string('Correo_Electronico')->unique()->comment("Correo electronico del usuario");
             $table->string('Contrasena')->comment("contraseña del usuario");
+            $table->enum('Tip_Estado',["A","B","N"])->comment("A = Activo , B = Bloquiado , N = Nuevo");
             $table->date('Fec_Cambio')->nullable()->comment("Fecha de cambio de contraseña"); 
-            $table->unsignedBigInteger('Cod_Estado_Usuario')->comment("Estado del usuario")->comment("LLave Foranea con el estado del usuario"); 
-            $table->foreign('Cod_Estado_Usuario')->references('Cod_Estado')->on('TBL_ESTADO_USUARIOS');
-            $table->unsignedBigInteger('Cod_Rol')->comment("Estado del usuario")->comment("LLave Foranea de los roles del usuario"); 
+            $table->unsignedBigInteger('Cod_Persona')->comment("LLave Foranea para la tabla de personas"); 
+            $table->foreign('Cod_Persona')->references('Cod_Persona')->on('TBL_PERSONAS');
+            //$table->unsignedBigInteger('Cod_Estado_Usuario')->comment("Estado del usuario")->comment("LLave Foranea con el estado del usuario"); 
+            // $table->foreign('Cod_Estado_Usuario')->references('Cod_Estado')->on('TBL_ESTADO_USUARIOS');
+            $table->unsignedBigInteger('Cod_Rol')->comment("LLave Foranea de los roles del usuario"); 
             $table->foreign('Cod_Rol')->references('Cod_Rol')->on('tbl_roles');
             //$table->timestamp('email_verified_at')->nullable();
             //$table->rememberToken();
